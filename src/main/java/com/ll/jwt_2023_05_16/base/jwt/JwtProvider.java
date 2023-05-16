9e76f20719afe03d531746565d977ec57bdfa192
Hjun1004 <hjun1004@naver.com>
2023-05-16 16:48:03 +0900

    토큰 발급 및 인증

---
+++ src/main/java/com/ll/jwt_2023_05_16/base/jwt/JwtProvider.java
@@ -0,0 +1,69 @@
+package com.ll.jwt_2023_05_16.base.jwt;
+
+import com.ll.jwt_2023_05_16.util.Ut;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
+
+@Component
+public class JwtProvider {
+    private SecretKey cachedSecretKey;
+
+    @Value("${custom.jwt.secretKey}")
+    private String secretKeyPlain;
+
+    private SecretKey _getSecretKey() {
+        String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
+        return Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
+    }
+
+    public SecretKey getSecretKey() {
+        if (cachedSecretKey == null) cachedSecretKey = _getSecretKey();
+
+        return cachedSecretKey;
+    }
+
+    public String genToken(Map<String, Object> claims, int seconds) {
+        long now = new Date().getTime();
+        Date accessTokenExpiresIn = new Date(now + 1000L * seconds);
+
+        return Jwts.builder()
+                .claim("body", Ut.json.toStr(claims))
+                .setExpiration(accessTokenExpiresIn)
+                .signWith(getSecretKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    //  토큰의 유효기간검사와 본인이 직접 토큰을 생성하였는지 체크한다.
+    public boolean verify(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSecretKey())
+                    .build()
+                    .parseClaimsJws(token);
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    // 토큰 내부의 내용을 볼 수 있는 메서드
+    public Map<String, Object> getClaims(String token) {
+        String body = Jwts.parserBuilder()
+                .setSigningKey(getSecretKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("body", String.class);
+
+        return Ut.json.toMap(body);
+    }
+}
